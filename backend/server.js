bc6bc3bedef0ec46e0e60f5ea518573ee76b5f47
Siddhu70 <siddharthwaghmare7071@gmail.com>
2026-05-19 22:42:41 +0530

    Updated production CORS

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,67 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import resumeRoutes from "./routes/resumeRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// ======================
+// CORS
+// ======================
+
+app.use(
+  cors({
+
+    origin: [
+      "http://localhost:5173",
+
+      "https://ai-resume-assistance-olive.vercel.app",
+    ],
+
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "DELETE",
+    ],
+
+    credentials: true,
+  })
+);
+
+// ======================
+// MIDDLEWARE
+// ======================
+
 app.use(express.json());
+
+// ======================
+// ROUTES
+// ======================
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-const PORT = process.env.PORT || 5000;
+app.use(
+  "/api/resume",
+  resumeRoutes
+);
+
+// ======================
+// SERVER
+// ======================
+
+const PORT =
+  process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
 
-app.use("/api/resume", resumeRoutes);
+  console.log(
+    `Server running on ${PORT}`
+  );
+
+});
